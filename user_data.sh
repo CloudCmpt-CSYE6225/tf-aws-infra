7ecd4a557007209ddc99584d1441c0a0cf2dd130
Srijith Makam <makam.s@northeastern.edu>
2024-10-30 19:51:54 -0400

    metrics interval update

--- user_data.sh
+++ user_data.sh
@@ -13,7 +13,7 @@ log "Starting user data script execution"
 cat > /tmp/cloudwatch-config.json << 'EOL'
 {
   "agent": {
-    "metrics_collection_interval": 60,
+    "metrics_collection_interval": 5,
     "run_as_user": "root"
   },
   "logs": {
@@ -42,28 +42,8 @@ cat > /tmp/cloudwatch-config.json << 'EOL'
     "metrics_collected": {
       "statsd": {
         "service_address": ":8125",
-        "metrics_collection_interval": 60,
-        "metrics_aggregation_interval": 60
-      },
-      "cpu": {
-        "measurement": [
-          "cpu_usage_idle",
-          "cpu_usage_user",
-          "cpu_usage_system"
-        ],
-        "metrics_collection_interval": 60
-      },
-      "mem": {
-        "measurement": [
-          "mem_used_percent"
-        ],
-        "metrics_collection_interval": 60
-      },
-      "disk": {
-        "measurement": [
-          "disk_used_percent"
-        ],
-        "metrics_collection_interval": 60
+        "metrics_collection_interval": 5,
+        "metrics_aggregation_interval": 5
       }
     }
   }
