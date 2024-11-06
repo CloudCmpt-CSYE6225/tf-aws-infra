360843b322e2a23188728be770a290ecb23621e8
Srijith Makam <makam.s@northeastern.edu>
2024-11-06 18:41:02 -0500

    tf fmt

--- network.tf
+++ network.tf
@@ -483,7 +483,7 @@ resource "aws_autoscaling_policy" "scale_up" {
   name                   = "scale-up-policy"
   autoscaling_group_name = aws_autoscaling_group.app_asg.name
   adjustment_type        = "ChangeInCapacity"
-  scaling_adjustment     = 1 
+  scaling_adjustment     = 1
   cooldown               = 60
 }
 
@@ -493,10 +493,10 @@ resource "aws_cloudwatch_metric_alarm" "scale_up_alarm" {
   evaluation_periods  = "2"
   metric_name         = "CPUUtilization"
   namespace           = "AWS/EC2"
-  period              = "60" 
+  period              = "60"
   statistic           = "Average"
-  threshold           = "9"                                   
-  alarm_actions       = [aws_autoscaling_policy.scale_up.arn] 
+  threshold           = "9"
+  alarm_actions       = [aws_autoscaling_policy.scale_up.arn]
 
   dimensions = {
     AutoScalingGroupName = aws_autoscaling_group.app_asg.name
@@ -517,10 +517,10 @@ resource "aws_cloudwatch_metric_alarm" "scale_down_alarm" {
   evaluation_periods  = "2"
   metric_name         = "CPUUtilization"
   namespace           = "AWS/EC2"
-  period              = "60" 
+  period              = "60"
   statistic           = "Average"
-  threshold           = "7"                                     
-  alarm_actions       = [aws_autoscaling_policy.scale_down.arn] 
+  threshold           = "7"
+  alarm_actions       = [aws_autoscaling_policy.scale_down.arn]
 
   dimensions = {
     AutoScalingGroupName = aws_autoscaling_group.app_asg.name
