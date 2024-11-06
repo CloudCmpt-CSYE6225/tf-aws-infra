90aa2e2e4da7bc1716eea7ac2859cafe7b488e25
Srijith Makam <makam.s@northeastern.edu>
2024-11-06 18:36:51 -0500

    updated cpu threshold

--- network.tf
+++ network.tf
@@ -483,7 +483,7 @@ resource "aws_autoscaling_policy" "scale_up" {
   name                   = "scale-up-policy"
   autoscaling_group_name = aws_autoscaling_group.app_asg.name
   adjustment_type        = "ChangeInCapacity"
-  scaling_adjustment     = 1 # Increase by one instance
+  scaling_adjustment     = 1 
   cooldown               = 60
 }
 
@@ -493,10 +493,10 @@ resource "aws_cloudwatch_metric_alarm" "scale_up_alarm" {
   evaluation_periods  = "2"
   metric_name         = "CPUUtilization"
   namespace           = "AWS/EC2"
-  period              = "60" # Check every minute
+  period              = "60" 
   statistic           = "Average"
-  threshold           = "5"                                   # Trigger when CPU exceeds 5%
-  alarm_actions       = [aws_autoscaling_policy.scale_up.arn] # Link to scale-up policy
+  threshold           = "9"                                   
+  alarm_actions       = [aws_autoscaling_policy.scale_up.arn] 
 
   dimensions = {
     AutoScalingGroupName = aws_autoscaling_group.app_asg.name
@@ -507,7 +507,7 @@ resource "aws_autoscaling_policy" "scale_down" {
   name                   = "scale-down-policy"
   autoscaling_group_name = aws_autoscaling_group.app_asg.name
   adjustment_type        = "ChangeInCapacity"
-  scaling_adjustment     = -1 # Decrease by one instance
+  scaling_adjustment     = -1
   cooldown               = 60
 }
 
@@ -517,10 +517,10 @@ resource "aws_cloudwatch_metric_alarm" "scale_down_alarm" {
   evaluation_periods  = "2"
   metric_name         = "CPUUtilization"
   namespace           = "AWS/EC2"
-  period              = "60" # Check every minute
+  period              = "60" 
   statistic           = "Average"
-  threshold           = "3"                                     # Trigger when CPU falls below 3%
-  alarm_actions       = [aws_autoscaling_policy.scale_down.arn] # Link to scale-down policy
+  threshold           = "7"                                     
+  alarm_actions       = [aws_autoscaling_policy.scale_down.arn] 
 
   dimensions = {
     AutoScalingGroupName = aws_autoscaling_group.app_asg.name
